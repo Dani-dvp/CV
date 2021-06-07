ae257b17a1276503729bd95c7f424860853464e3
Dani-dvp <kevin1995dani@gmail.com>
2021-06-07 10:05:30 +0200

    Updates

---
+++ precache-manifest.bb458a2f252efdb3fee3a44b82bf336e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11432cf2b39c7fad3017fac6872ba3d2",
+    "url": "/CV/index.html"
+  },
+  {
+    "revision": "1bcc11b47c6a2f2e6372",
+    "url": "/CV/static/css/2.87a9c7a2.chunk.css"
+  },
+  {
+    "revision": "ca6a972b68028694997e",
+    "url": "/CV/static/css/main.7c8d07de.chunk.css"
+  },
+  {
+    "revision": "1bcc11b47c6a2f2e6372",
+    "url": "/CV/static/js/2.763b96fc.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/CV/static/js/2.763b96fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca6a972b68028694997e",
+    "url": "/CV/static/js/main.0a934913.chunk.js"
+  },
+  {
+    "revision": "54b6bfd6263f0e7f8687",
+    "url": "/CV/static/js/runtime-main.7ad843e0.js"
+  },
+  {
+    "revision": "10df0266782ac5b42831a2e8910dad10",
+    "url": "/CV/static/media/Jag.10df0266.jpg"
+  }
+]);
